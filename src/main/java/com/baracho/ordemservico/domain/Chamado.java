f3452e55b9daca2b77dfefbb1512b9762a71b64b
Baracho <baracholss@gmail.com>
2023-07-03 15:57:45 -0300

    anotacoes jpa para classes de dominio

--- src/main/java/com/baracho/ordemservico/domain/Chamado.java
+++ src/main/java/com/baracho/ordemservico/domain/Chamado.java
@@ -1,17 +1,34 @@
 package com.baracho.ordemservico.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import com.baracho.ordemservico.domain.enums.Prioridade;
 import com.baracho.ordemservico.domain.enums.Status;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
-public class Chamado {
+@Entity
+public class Chamado implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dateAbertura = LocalDate.now();
 	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataFechamento;
+	
 	
 	private Prioridade prioridade;
 	
@@ -21,8 +38,12 @@ public class Chamado {
 	
 	private String observacoes;
 	
+	@ManyToOne
+	@JoinColumn(name = "tecnico_id")
 	private Tecnico tecnico;
 	
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
 
 	public Chamado() {
