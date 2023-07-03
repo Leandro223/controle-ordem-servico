76d3637953578702c4f4f12e831dcffa800bd377
Baracho <baracholss@gmail.com>
2023-07-03 14:52:35 -0300

    repositorio local e remoto

---
+++ src/main/java/com/baracho/ordemservico/ControleOrdemServicoApplication.java
@@ -0,0 +1,13 @@
+package com.baracho.ordemservico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleOrdemServicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleOrdemServicoApplication.class, args);
+	}
+
+}
