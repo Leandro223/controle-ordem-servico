ce64486b50405e1f8f1e662002baaa8ee69f9aa0
Baracho <baracholss@gmail.com>
2023-07-04 16:05:08 -0300

    endpoint findAll para chamado

--- src/main/java/com/baracho/ordemservico/service/ChamadoService.java
+++ src/main/java/com/baracho/ordemservico/service/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.baracho.ordemservico.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,11 @@ public class ChamadoService {
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = chamadoRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado para o id: " + id));
+	}
+
+	public List<Chamado> findAll() {
+		
+		return chamadoRepository.findAll();
 	}
 
 }
