2931179908d9dcbed061227b87101926181f99a2
Baracho <baracholss@gmail.com>
2023-07-04 16:01:12 -0300

    endpoint findById para chamado

---
+++ src/main/java/com/baracho/ordemservico/service/ChamadoService.java
@@ -0,0 +1,23 @@
+package com.baracho.ordemservico.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.baracho.ordemservico.domain.Chamado;
+import com.baracho.ordemservico.repository.ChamadoRepository;
+import com.baracho.ordemservico.service.exceptions.ObjectnotFoundException;
+
+@Service
+public class ChamadoService {
+	
+	@Autowired 
+	private ChamadoRepository chamadoRepository;
+
+	public Chamado findById(Integer id) {
+		Optional<Chamado> obj = chamadoRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado para o id: " + id));
+	}
+
+}
