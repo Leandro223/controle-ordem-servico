db595fa8c29c46564c65bc7aebcb712d45ff2606
Baracho <baracholss@gmail.com>
2023-07-04 14:15:27 -0300

    metodo findAll para tecnico

--- src/main/java/com/baracho/ordemservico/service/TecnicoService.java
+++ src/main/java/com/baracho/ordemservico/service/TecnicoService.java
@@ -1,5 +1,6 @@
 package com.baracho.ordemservico.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,10 @@ public class TecnicoService {
 		
 		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado para o id: " + id));
 		
+	}
+
+	public List<Tecnico> findAll() {
+		return tecnicoRepository.findAll();
 	}
 
 }
